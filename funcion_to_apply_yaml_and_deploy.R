d11fc6658cb77b84018a63b14197473d6eb45728
Matt Brigida <matt@complete-markets.com>
2016-08-22 10:28:15 -0400

    Mobile IFC pres and starting deply function

---
+++ funcion_to_apply_yaml_and_deploy.R
@@ -0,0 +1,7 @@
+### We have presentations that we want to deploy as both isoslides and as html documents (which play nice on mobile).  However we want to maintain one .Rmd file.  So this function will prepend the appropriate YAML header to the Rmd and deploy.
+
+
+chooseDeploy <- function(yamlFile, rmdFile, theappName){
+
+
+    }
