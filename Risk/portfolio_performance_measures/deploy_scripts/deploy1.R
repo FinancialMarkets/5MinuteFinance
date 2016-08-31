c840cb93803d22d633080d9e0590f026e096f8bf
Matt Brigida <matt@complete-markets.com>
2016-08-31 10:03:09 -0400

    Deployed port performance measures

---
+++ Risk/portfolio_performance_measures/deploy_scripts/deploy1.R
@@ -0,0 +1,2 @@
+setwd("./deploy1_folder")
+rsconnect::deployApp(account = "micfm", appName = "portfolio_performance_measures")
---
+++ Risk/portfolio_performance_measures/deploy_scripts/deploy2.R
@@ -0,0 +1,2 @@
+setwd("./deploy2_folder")
+rsconnect::deployApp(account = "micfm", appName = "portfolio_performance_measures_mobile")
