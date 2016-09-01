11118f572505bc3fcc8eb7f2520b4db2b887e5fc
Matt Brigida <matt@complete-markets.com>
2016-09-01 13:00:38 -0400

    Deployed why is YTM used.

---
+++ Fixed_Income_and_Debt/For_What_YTM_Used/deploy_scripts/deploy1.R
@@ -0,0 +1,2 @@
+setwd("./deploy1_folder")
+rsconnect::deployApp(account = "micfm", appName = "For_What_YTM_Used")
---
+++ Fixed_Income_and_Debt/For_What_YTM_Used/deploy_scripts/deploy2.R
@@ -0,0 +1,2 @@
+setwd("./deploy2_folder")
+rsconnect::deployApp(account = "micfm", appName = "For_What_YTM_Used_mobile")
