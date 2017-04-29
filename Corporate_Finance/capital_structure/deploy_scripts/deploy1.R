68bfbdd437345cb39a464cbb8bc6e913e168a1ae
Matt Brigida <matt@complete-markets.com>
2017-04-29 13:49:25 -0400

    Deployment files for capital structure

---
+++ Corporate_Finance/capital_structure/deploy_scripts/deploy1.R
@@ -0,0 +1,2 @@
+setwd("./deploy1_folder")
+rsconnect::deployApp(account = "micfm", appName = "capital_structure")
---
+++ Corporate_Finance/capital_structure/deploy_scripts/deploy2.R
@@ -0,0 +1,2 @@
+setwd("./deploy2_folder")
+rsconnect::deployApp(account = "micfm", appName = "capital_structure_mobile")
