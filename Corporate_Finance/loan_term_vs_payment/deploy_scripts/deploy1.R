b96de84b917d477dc887e3f67d1d623cdb2ee6cb
Matt Brigida <matt@complete-markets.com>
2017-08-24 16:55:45 -0400

    loan term rate and payment pres almost done

---
+++ Corporate_Finance/loan_term_vs_payment/deploy_scripts/deploy1.R
@@ -0,0 +1,2 @@
+setwd("./deploy1_folder")
+rsconnect::deployApp(account = "micfm", appName = "loan_term_payment")
---
+++ Corporate_Finance/loan_term_vs_payment/deploy_scripts/deploy2.R
@@ -0,0 +1,2 @@
+setwd("./deploy2_folder")
+rsconnect::deployApp(account = "micfm", appName = "loan_term_payment_mobile")
